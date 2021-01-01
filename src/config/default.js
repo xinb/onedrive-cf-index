443d031abeb9986c83556f8a67af2b3a2ce38e18
xinb <netwhilt@outlook.com>
2021-01-01 14:05:33 +0800

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -5,14 +5,14 @@ const config = {
    * to get following params: client_id, client_secret, refresh_token & redirect_uri.
    */
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: 'e3cd4a8c-c2d1-429f-9d01-ef42f1dff787',
   client_secret: CLIENT_SECRET,
   redirect_uri: 'https://heymind.github.io/tools/microsoft-graph-api-auth',
 
   /**
    * The base path for indexing, all files and subfolders are public by this tool. For example: `/Public`.
    */
-  base: '/Public',
+  base: '',
 
   /**
    * Feature: add OneDriveCN (21Vianet) support
