08243714b1b2b0b4a8fe5bacc48cf2bde5b934fb
xinb <netwhilt@outlook.com>
2021-01-01 16:18:17 +0800

    Update folderView.js

--- src/folderView.js
+++ src/folderView.js
@@ -50,9 +50,9 @@ export async function renderFolderView(items, path, request) {
     )
 
   const intro = `<div class="intro markdown-body" style="text-align: left; margin-top: 2rem;">
-                    <h2>Yoo, I'm Spencer Woo 👋</h2>
-                    <p>This is Spencer's OneDrive public directory listing. Feel free to download any files that you find useful. Reach me at: spencer.wushangbo [at] gmail [dot] com.</p>
-                    <p><a href="https://spencerwoo.com">Portfolio</a> · <a href="https://blog.spencerwoo.com">Blog</a> · <a href="https://github.com/spencerwooo">GitHub</a></p>
+                    <h2>Yoo, I'm Xinb 👋</h2>
+                    <p>This is Spencer's OneDrive public directory listing. Feel free to download any files that you find useful. Reach me at: ******** [at] gmail [dot] com.</p>
+                    <p><a href="https://xinb.github.com">Portfolio</a> · <a href="https://xini.me">Blog</a> · <a href="https://github.com/xinb">GitHub</a></p>
                   </div>`
 
   // Check if current directory contains README.md, if true, then render spinner
